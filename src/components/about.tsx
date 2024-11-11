98f3d83dcf61ac844520f54ac62f84f5bf854145
Theo Cooper-Brown <trcb.github@gmail.com>
2024-11-11 11:37:33 +0000

    update about

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -7,15 +7,15 @@ function About() {
         </h2>
         <p>
           Results-driven software developer with a unique blend of financial
-          acumen and technology expertise. Leveraging a strong academic in
-          Accounting & Finance and professional experience in IT Audit, I bring
-          an analytical approach to problem-solving in software development.
-          Recently completed an intensive 3-month Codeworks Software Engineering
-          bootcamp, gaining hands-on experience with JavaScript/TypeScript,
-          React, Next.js, and Express.js through immersive 72-hour weeks. Eager
-          to apply my diverse skill set, passion for continuous learning, and
-          technical skills to make an immediate impact in a dynamic development
-          team.
+          acumen and technology expertise. Leveraging a strong academic
+          foundation in Accounting & Finance and professional experience in IT
+          Audit, I bring an analytical approach to problem-solving in software
+          development. Recently completed an intensive 3-month Codeworks
+          Software Engineering bootcamp, gaining hands-on experience with
+          JavaScript/TypeScript, React, Next.js, and Express.js through
+          immersive 72-hour weeks. Eager to apply my technical skills and
+          passion for continuous learning to make an immediate impact in a
+          dynamic development team.
         </p>
       </div>
     </div>
